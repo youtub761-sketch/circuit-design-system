ef53b1e8cd716387cdb0a24207a2e8cc46f11b64
youtub761-sketch <youtub761@gmail.com>
2026-06-28 16:55:23 +0800

    Update circuit_generator.py

--- circuit_generator.py
+++ circuit_generator.py
@@ -14,24 +14,9 @@ def get_circuit_drawing(equations):
         IcPin(name='K0', side='left', slot='7/8')
     ], name='JK-FF'))
     
-    def draw_logic(d, target_pin, eq):
-        eq = eq.replace(' ', '')
-        if '&' in eq:
-            parts = eq.split('&')
-            g = d.add(logic.And(inputs=2).right().at(target_pin).anchor('out'))
-            d.add(logic.Line().at(g.in1).left().label(parts[0], loc='left', color='white'))
-            d.add(logic.Line().at(g.in2).left().label(parts[1], loc='left', color='white'))
-        elif '|' in eq:
-            parts = eq.split('|')
-            g = d.add(logic.Or(inputs=2).right().at(target_pin).anchor('out'))
-            d.add(logic.Line().at(g.in1).left().label(parts[0], loc='left', color='white'))
-            d.add(logic.Line().at(g.in2).left().label(parts[1], loc='left', color='white'))
-        else:
-            d.add(logic.Line().at(target_pin).left().label(eq, loc='left', color='white'))
-
-    draw_logic(d, jk.J1, equations.get('J1', '0'))
-    draw_logic(d, jk.K1, equations.get('K1', '0'))
-    draw_logic(d, jk.J0, equations.get('J0', '0'))
-    draw_logic(d, jk.K0, equations.get('K0', '0'))
+    # ... (這裡保留你原本定義的 draw_logic 函數) ...
     
-    return d  # 【關鍵修改】：回傳 drawing 物件給網頁
+    # 執行繪製邏輯...
+    # (確保這裡沒有 d.save() 或其他繪圖指令)
+    
+    return d  # 這裡只回傳 Drawing 物件，不要呼叫 draw()
